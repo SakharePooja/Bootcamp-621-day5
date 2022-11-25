cde35dcecd2c550c8d8bf953ea00a477c660ccb0
SakharePooja <poojasakhare94@gmail.com>
2022-11-25 17:01:41 +0530

    added:working hour partime

--- ifloop.sh
+++ ifloop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-isPresent=$((RANDOM%2));
+isPresent=$((RANDOM%3));
 perHourSalary=20;
 workingHour=0;
 
@@ -8,10 +8,16 @@ if [ $isPresent -eq 0 ]
 then
 	echo "Employee is Absent";
 	workingHour=0;
-else 
+elif [ $isPresent -eq 1 ]
+then
 	echo "Employee is present";
 	workingHour=8;
-
+else
+	echo "Employee is working as part time";
+	workingHour=4;
 
 fi
 
+salary=$(($perHourSalary * $workingHour));
+
+echo "Employee is earned $salary USD Today";
